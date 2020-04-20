a9061c87cbb713aba6ed6295acfb869774297fbd
Muddassar S Shaikh <shaikh.muddassar8@gmail.com>
2020-04-20 15:07:02 +0530

    Created node server code on port from config file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,52 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import * as path from 'path';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
-const port = 3000;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+// HTTP request logger middleware for node.js
+// app.use(morgan('combined', { stream: winston.stream }));
+app.use(morgan('combined'));
+
+/**
+ * Parse incoming request bodies in a middleware before your handlers,
+ * available under the req.body property.
+ */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse Cookie header and populate req.cookies
+app.use(cookieParser());
+
+/**
+ * CORS is a node.js package for providing a Connect/Express middleware
+ * that can be used to enable CORS with various options.
+ */
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * apply to all requests
+ * Note - Rate Limiter can be applied to any individual API also. For more information
+ * Please visit https://www.npmjs.com/package/express-rate-limit
+ */
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+
 app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
 });
-app.listen(port, (err) => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on ${port}`);
-});
+
+export default app;
---
+++ src/config.ts
@@ -0,0 +1,20 @@
+const config = {
+  portNumber: 6786,
+  databaseHost: 'localhost',
+  databaseUser: 'root',
+  databasePassword: '',
+  databaseDatabaseName: 'commondb',
+  databasePort: 3306,
+  mongoDBConnectionString: 'mongodb://localhost:27017/commonDB',
+  emailVerifiedLink: 'http://localhost:4200/confirmEmail?token=',
+  resetPasswordLink: 'http://localhost:4200/resetPassword?token=',
+  tokenkey: '37LvPsm4vaBcd4CY',
+  env: 'development',
+  supportEmail: 'info@commonAPI.com',
+  SMTPemailAddress: 'developers.winjit@gmail.com',
+  SMTPPassword: 'Winjit@321',
+  database_initial: 'common',
+  database: 'mysql',
+};
+
+export default config;
